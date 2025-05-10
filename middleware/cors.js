8e9ac48ce4f086741a44133506828423bf1cdb52
kiniorap <li.carlosduarte98@gmail.com>
2025-05-10 13:50:20 -0700

    se modifica el cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,23 +1,19 @@
 export const corsMiddleware = (req, res) => {
-  const allowedOrigins = (process.env.FRONTEND_ORIGINS || "")
-    .split(",")
-    .map((origin) => origin.trim());
-
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  } else {
-    res.status(403).json({ error: "Origen no permitido" });
-    return false;
+  const allowedOrigin = "";
+  if (req.headers.origin === process.env.FRONTEND_ORIGINS) {
+    allowedOrigin = process.env.FRONTEND_ORIGINS;
+  } else if (req.headers.origin === "http://localhost:5173") {
+    allowedOrigin = process.env.FRONTEND_ORIGINS;
   }
+
+  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
     res.status(200).end();
-    return false; 
+    return false; // corta la ejecución
   }
 
-  return true; 
-}
+  return true; // continuar con el handler principal
+};
