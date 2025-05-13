c66c85752a529238fd828233f4501f53cd6b5f02
kiniorap <li.carlosduarte98@gmail.com>
2025-05-12 18:59:32 -0700

    se actualiza para que no acepte peticiones desde el localhost para que no aumente el contador

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,9 +2,9 @@ export const corsMiddleware = (req, res) => {
   let  allowedOrigin = "";
   if (req.headers.origin === process.env.FRONTEND_ORIGINS) {
     allowedOrigin = process.env.FRONTEND_ORIGINS;
-  } else if (req.headers.origin === "http://localhost:5173") {
+  }/* else if (req.headers.origin === "http://localhost:5173") {
     allowedOrigin = process.env.FRONTEND_ORIGINS;
-  }
+  }*/
 
   res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
